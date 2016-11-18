6707ea6cdff0ab8318ab7b03fb6016468c7be250
ee2108 <edruehl@yahoo.com>
2016-11-18 16:09:27 -0500

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+# Girls-who-Code
+
+public class HelloWorld {
+   public static void main(String[] args) {
+      //create student named Karlie
+      Student karlie = new Student("Karlie Kloss", 113587313, 3.2, 4);
+
+      //have karlie take a test
+      karlie.takeTest();
+   }
+}
